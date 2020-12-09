a271ade1f5bc60cb089e821f72e1265fa3bbaf10
bsuverzabaek <69542867+bsuverzabaek@users.noreply.github.com>
2020-12-09 15:33:44 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+//import Vue from 'vue'
+//import { createApp } from 'vue'
+//import App from './App.vue'
+
+
+
+/*const HelloVueApp = {
+	data() {
+		return {
+			message: 'Hello Vue!!'
+		}
+	}
+}
+
+Vue.createApp(HelloVueApp).mount('#app')*/
